5c274f6934119d299ba30c4c65ed23c233dd630d
eranshachar <eranshachar14@gmail.com>
2020-04-30 21:26:14 +0300

    updated create instance script

--- GCP/02-create-instance.sh
+++ GCP/02-create-instance.sh
@@ -5,4 +5,5 @@
 source lib/gcloud-library.sh
 
 # create_instance NAME SUBNET DISK_SIZE
-create_vm_instance 'instance-3' 'default' '20GB'
+create_vm_instance 'instance-2' 'testnet-subnet-01' '20GB'
+create_vm_instance 'instance-3' 'testnet-subnet-02' '20GB'
