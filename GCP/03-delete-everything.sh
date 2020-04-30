9dc059c101f7e92b516c6615e1b5e79e38c6c213
eranshachar <eranshachar14@gmail.com>
2020-04-30 16:07:23 +0300

    added a script to delete instance, subnet. and network

--- GCP/03-delete-everything.sh
+++ GCP/03-delete-everything.sh
@@ -1,4 +1,13 @@
+#!/usr/bin/env bash
+# Use this script to delete Instance, Subnet and Network
 
-delete_instance 'INSTANCE'
-delete_subnet 'SUBNET'
-delete_network 'NETWORK'
+# Take in all our functions for GCP
+source lib/gcloud-library.sh
+
+# delete_subnet: SUBNET_NAME
+# delete_instance: INSTANCE
+# delete_network: NETWORK
+
+delete_vm_instance 'instance-3'
+delete_subnet 'eransub'
+delete_network 'testnet'
