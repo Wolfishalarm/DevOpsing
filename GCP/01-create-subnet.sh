3a021f3101b1c3b40a6a9f0615c7cc4b087125d6
eranshachar <eranshachar14@gmail.com>
2020-04-30 21:25:42 +0300

    updated subnet script and added vim.md

--- GCP/01-create-subnet.sh
+++ GCP/01-create-subnet.sh
@@ -5,4 +5,11 @@
 source lib/gcloud-library.sh
 
 # create_subnet SUBNET NETWORK RANGE
-create_subnet 'eransub' 'mynet' '255.255.255.0/20'
+create_subnet 'testnet-subnet-01' 'testnet' '192.168.86.0/24'
+create_subnet 'testnet-subnet-02' 'testnet' '10.20.0.0/24'
+
+# 255.255.255.0 = SUBNET MASK (read about this)
+# create_subnet 'eransub' 'mynet' '192.168.86.0/20'
+# create_subnet 'eransub' 'mynet' '172.20.0.0/24'
+# create_subnet 'eransub' 'mynet' '10.20.0.0/24'
+# Read about private ip space (RFC1918)
---
+++ vim.md
@@ -0,0 +1,9 @@
+## This is my vim readme file with tips and the basics.
+
+### Tips
+
+### yank lines, then paste
+```
+  :y4 (yank 4 lines)
+  p   (paste)
+```
